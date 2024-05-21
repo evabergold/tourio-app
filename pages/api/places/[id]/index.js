a665bc031fc6ea49f3478a9f5d35e0e89e867f14
Eva Bergold <evabergold@gmail.com>
2024-05-21 12:18:18 +0200

    initial commit

---
+++ pages/api/places/[id]/index.js
@@ -0,0 +1,31 @@
+import Place from "@/db/models/Place";
+import dbConnect from "@/db/connect";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (!id) {
+    return;
+  }
+
+  if (request.method === "GET") {
+    console.log(id);
+    const place = await Place.findById(id);
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+    response.status(200).json(place);
+  }
+
+  if (request.method === "PATCH") {
+    const placeData = request.body;
+    await Place.findByIdAndUpdate(id, placeData);
+    response.status(200).json({ status: "Place updated" });
+  }
+
+  if (request.method === "DELETE") {
+    await Place.findByIdAndDelete(id);
+    response.status(200).json({ status: "Joke deleted" });
+  }
+}
---
+++ pages/api/places/index.js
@@ -0,0 +1,24 @@
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
+//import { places } from "../../../lib/db";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      return response.status(201).json({ status: "Place created." });
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
